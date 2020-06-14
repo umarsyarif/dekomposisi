362ae9898c46d97ac8274048db5f5df28b1b8e51
Windows <11750115076@students.uin-suska.ac.id>
2020-06-14 07:14:37 +0000

    seeder

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'HomeController@home')->name('home');
+Route::get('/', 'HomeController@home')->name('page');
 
 Auth::routes();
 
